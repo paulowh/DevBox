22ed14683121c285dec8311d9b5aa62f5675b54e
Paulo Santos <ph744915@gmail.com>
2025-11-22 06:43:57 -0300

    removendo dependencias obsoletas

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,11 +1,4 @@
-// DevBox - Aplicação Principal
-// jQuery e Fomantic UI carregados via CDN
-
-$(document).ready(function() {
-  console.log("DevBox carregado com sucesso!");
-  
-  // Inicializa componentes Fomantic UI
-  $('.ui.dropdown').dropdown();
-  $('.ui.modal').modal();
-  $('.ui.accordion').accordion();
+// Teste
+$(document).ready(() => {
+  console.log("Vite + PHP + Twig + Fomantic funcionando!");
 });
