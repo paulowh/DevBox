664f1bf1a17275a9189ee51108e2dbc376fadaf6
Paulo Santos <ph744915@gmail.com>
2025-11-24 17:50:27 -0300

    feat: Criado função de request para as UC

--- app/Controllers/UcController.php
+++ app/Controllers/UcController.php
@@ -3,34 +3,25 @@
 namespace App\Controllers;
 
 use App\Core\View;
+use App\Models\Atitude;
+use App\Models\Conhecimento;
 use App\Models\Curso;
+use App\Models\Habilidade;
+use App\Models\Indicador;
+use App\Models\Uc;
 
 class UcController
 {
   public function index()
   {
-    // formatar nesse modelo
-    $ucs = [
-      [
-        'id' => 1,
-        'nome' => 'UC1',
-        'dados' => 'Dados da UC1...',
-        'indicadores' => ['UC1 - Indicador 1', 'UC1 - Indicador 2'],
-        'conhecimentos' => ['UC1 - Conhecimento 1', 'UC1 - Conhecimento 2'],
-        'habilidades' => ['UC1 - Habilidade 1', 'UC1 - Habilidade 2'],
-        'atitudes' => ['UC1 - Atitude 1', 'UC1 - Atitude 2'],
-      ],
-      [
-        'id' => 2,
-        'nome' => 'UC2',
-        'dados' => 'Dados da UC2...',
-        'indicadores' => ['UC2 - Indicador 1', 'UC2 - Indicador 2'],
-        'conhecimentos' => ['UC2 - Conhecimento 1', 'UC2 - Conhecimento 2'],
-        'habilidades' => ['UC2 - Habilidade 1', 'UC2 - Habilidade 2'],
-        'atitudes' => ['UC2 - Atitude 1', 'UC2 - Atitude 2'],
-      ],
-    ];
+    $ucs = Uc::all();
 
+    foreach ($ucs as $uc) {
+      $uc->indicadores = Indicador::where('uc_id', $uc->id)->get();
+      $uc->conhecimentos = Conhecimento::where('uc_id', $uc->id)->get();
+      $uc->habilidades = Habilidade::where('uc_id', $uc->id)->get();
+      $uc->atitudes = Atitude::where('uc_id', $uc->id)->get();
+    }
     echo View::make('uc/listar', ['ucs' => $ucs]);
   }
 
